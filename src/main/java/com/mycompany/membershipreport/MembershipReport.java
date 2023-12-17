ea58a8acde6f6452689ec373834c83d9e48d8e7e
saiifulhilal <hilalasw@gmail.com>
2023-12-17 19:27:57 +0700

    Jasper

---
+++ src/main/java/com/mycompany/membershipreport/MembershipReport.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.membershipreport;
+
+/**
+ *
+ * @author shila
+ */
+public class MembershipReport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
